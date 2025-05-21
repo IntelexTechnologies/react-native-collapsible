7da1e6dffa63df6288cb422e26effe0e4c0d1c6e
Saurav Dutta <saurav.dutta@capgemini.com>
2025-05-21 18:27:37 +0530

    Revert "Update Collapsible.js"
    This reverts commit 0c974020906c604c8087ec78d11bcc01f4fe2cad.

--- Collapsible.js
+++ Collapsible.js
@@ -28,9 +28,7 @@ export default class Collapsible extends Component {
 
   componentDidMount() {
     if (!this.props.collapsed) {
-      setTimeout(() => {
-        this._measureContent((height) => this.state.height.setValue(height));
-      }, [1000])
+      this._measureContent((height) => height != null && this.state.height.setValue(height));
     }
   }
 
